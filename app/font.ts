92cb1b51393dac75d53df96d290d351ddbd4174f
황수영 <blythe@jstair.com>
2023-06-15 16:14:24 +0900

    font 변경

--- app/font.ts
+++ app/font.ts
@@ -1,23 +1,8 @@
-import { Sunflower, Gamja_Flower, Noto_Serif_KR, Noto_Sans_KR } from "next/font/google";
-
-const sun = Sunflower({
-    weight: ["300", "500", "700"],
-    subsets: ["latin"],
-});
-
-const gamja = Gamja_Flower({
-    weight: "400",
-    subsets: ["latin"],
-});
-
-const notoSerif = Noto_Serif_KR({
-    weight: ["300", "400", "500", "600", "700"],
-    subsets: ["latin"],
-});
+import { Noto_Sans_KR } from "next/font/google";
 
 const notoSans = Noto_Sans_KR({
     weight: ["300", "400", "500", "700"],
     subsets: ["latin"],
 });
 
-export { sun, gamja, notoSerif, notoSans };
+export { notoSans };
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import "../styles/globals.css";
 import Header from "./component/layout/Header";
 import Footer from "./component/layout/Footer";
-import { sun } from "./font";
+import { notoSans } from "./font";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
@@ -11,7 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </head>
-            <body className={sun.className}>
+            <body className={notoSans.className}>
                 <Header />
                 <div className="container py-4">{children}</div>
                 <Footer />
