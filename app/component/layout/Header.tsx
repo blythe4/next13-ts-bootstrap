45fa31c300da863ace6db5a553a569f0ab53f380
황수영 <blythe@jstair.com>
2023-06-19 16:47:45 +0900

    header active 적용

--- app/component/layout/Header.tsx
+++ app/component/layout/Header.tsx
@@ -1,27 +1,52 @@
 "use client";
 
 import React from "react";
-import { Container, Offcanvas, Navbar, Nav } from "react-bootstrap";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { GiMonsteraLeaf } from "react-icons/gi";
+import styled from "styled-components";
+
+const Navigation = [
+    {
+        path: "/garden",
+        name: "실내정원",
+    },
+    {
+        path: "/therpy",
+        name: "약초",
+    },
+    {
+        path: "/weeds",
+        name: "잡초",
+    },
+];
 
 function Header() {
-    const expand = "lg";
+    const pathName = usePathname();
     return (
         <Navbar expand="lg">
             <Container>
-                <Navbar.Brand href="/">식집사</Navbar.Brand>
+                <Logo>
+                    <Link href="/" className="d-inline-flex align-items-center">
+                        식집사
+                        <GiMonsteraLeaf />
+                    </Link>
+                </Logo>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Item>
-                            <Nav.Link href="/garden">실내정원</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href="/therpy">약초</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href="/weeds">잡초</Nav.Link>
-                        </Nav.Item>
-                    </Nav>
+                    <NavList className="me-auto gap-3">
+                        {Navigation.map((nav, idx) => (
+                            <NavItem
+                                key={idx}
+                                className={`rounded ${nav.path === pathName && "bg-success text-light"}`}
+                            >
+                                <Link href={nav.path} className="d-block">
+                                    {nav.name}
+                                </Link>
+                            </NavItem>
+                        ))}
+                    </NavList>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
@@ -29,3 +54,20 @@ function Header() {
 }
 
 export default Header;
+
+const Logo = styled.div`
+    padding: 10px 20px;
+    font-size: 32px;
+`;
+
+const NavList = styled(Nav)`
+    a {
+        padding: 8px;
+    }
+`;
+
+const NavItem = styled(Nav.Item)`
+    a {
+        display: block;
+    }
+`;
